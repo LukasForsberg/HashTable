ea4ca63a4d13238dfc7b8b0b0e00d7bf78bdf9f4
Lukas <lukas96.forsberg@gmail.com>
2019-11-07 10:29:36 +0100

    done again

--- firstIter/HashTable.cpp
+++ firstIter/HashTable.cpp
@@ -6,13 +6,13 @@ HashTable<Key, Value>::HashTable(){
 }
 
 template<class Key, class Value>
-void HashTable<Key, Value>::singleWrite(Key& key, Value& value){
+void HashTable<Key, Value>::singleWrite(Key key, Value value){
   int index = hash_func(key);
   buckets[index].push_front(make_pair(key, value));
 }
 
 template<class Key, class Value>
-const Value* HashTable<Key, Value>::singleRead(Key& key){
+const Value* HashTable<Key, Value>::singleRead(Key key){
 
   size_t index = hash_func(key);
 
@@ -32,7 +32,7 @@ const Value* HashTable<Key, Value>::singleRead(Key& key){
 }
 
 template<class Key, class Value>
-size_t HashTable<Key, Value>::hash_func(Key& key){
+size_t HashTable<Key, Value>::hash_func(Key key){
   hash<Key> h;
   return (h(key) % buckets.size());
 }
