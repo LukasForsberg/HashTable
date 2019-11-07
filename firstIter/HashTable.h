1e2d0ccbaff6da6232469963370a3b67d6790eff
Lukas <lukas96.forsberg@gmail.com>
2019-11-07 10:27:39 +0100

    done with implementation

--- firstIter/HashTable.h
+++ firstIter/HashTable.h
@@ -14,10 +14,10 @@ template<class Key, class Value> class HashTable {
 
   public:
     HashTable();
-    void singleWrite(Key key, Value value);
-    const Value* singleRead(Key key);
+    void singleWrite(Key& key, Value& value);
+    const Value* singleRead(Key& key);
     size_t size();
-    size_t hash_func(Key key);
+    size_t hash_func(Key& key);
 
   private:
     vector<forward_list<pair<Key,Value>>> buckets;
