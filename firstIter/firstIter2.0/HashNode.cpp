8018cd5a171b701c3d6a3533bb6f3ea8e6a549d9
Edvin Andersson <elt15ean@mars-11.e.lth.lu.se>
2019-11-12 10:11:59 +0100

    Upated hashnodes

--- firstIter/firstIter2.0/HashNode.cpp
+++ firstIter/firstIter2.0/HashNode.cpp
@@ -5,7 +5,7 @@ template<class Key, class Value>
 HashNode<Key, Value>::HashNode(Key k, Value val){
   key = k;
   value = val;
-  next =4 nullptr;
+  next = nullptr;
 }
 
 template<class Key, class Value>
